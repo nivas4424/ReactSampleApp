8a8ddcae71583d59ebafb0fcf6c4589c042371a9
nivas <vasu4424@gmail.com>
2017-02-10 16:30:58 -0500

    used arrow and stateless fn

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,11 +1,17 @@
 var React = require('react');
 
-var About = React.createClass({
-    render: function(){
-        return(
+// var About = React.createClass({
+//     render: function(){
+//         return(
+//             <h4>in About page</h4>
+//         );
+//     }
+// });
+
+var About = () =>{
+    return(
             <h4>in About page</h4>
         );
-    }
-});
+};
 
 module.exports = About;
